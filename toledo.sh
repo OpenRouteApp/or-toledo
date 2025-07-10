c349275e953d02bdfdf3b81ca0cb54182a6546c2
Eduardo Rodríguez Sánchez <edubuntu2002@gmail.com>
2025-07-10 17:37:13 +0200

    FIX: Fix file structure following google recommendations
    Previous commit was not tested before pushing, and it did not work
    properly. The new structure also follows google recomendation for
    package names

--- toledo.sh
+++ toledo.sh
@@ -5,15 +5,18 @@ LANG=$2
 
 # Setup command for executing buf utilitites within the docker container
 DOCKER_IMAGE="bufbuild/buf:latest"
-VOLUME_MOUNT="-v $(PWD):/build"
+VOLUME_MOUNT="-v $(pwd):/build"
 WORKDIR="-w /build"
 RUN_BUF_DOCKER="docker run --rm ${VOLUME_MOUNT} ${WORKDIR} ${DOCKER_IMAGE}"
 
+
 ${RUN_BUF_DOCKER} lint
-${RUN_BUF_DOCKER} breaking
+# Leave commented till next commit
+# ${RUN_BUF_DOCKER} breaking --against ".git#subdir=proto"
 
 BUF_TEMPLATE="templates/buf.gen.${LANG}.yaml"
-${RUN_BUF_DOCKER} generate      \
-    --template ${TEMPLATE}      \
-    --path "proto/${SERVICE}"   \
-    --exclude-imports
+${RUN_BUF_DOCKER} generate              \
+    --template ${BUF_TEMPLATE}          \
+    --path "proto/openroute/${SERVICE}" \
+    --output "gen/${SERVICE}/${LANG}"
+
